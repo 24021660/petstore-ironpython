9ec68d71e586254406fe778ef1118fd1eeb4d468
24021660 <24021660@qq.com>
2019-03-01 01:11:11 +0800

    1111111111111111

---
+++ petstore/app_main.py
@@ -0,0 +1,14 @@
+import wx
+
+from com.tony.petstore.ui.login_frame import LoginFrame
+
+
+class App(wx.App):
+    def OnInit(self):
+        frame=LoginFrame()
+        frame.Show()
+        return True
+  
+if __name__=='__main__':
+    app=App()
+    app.MainLoop()
